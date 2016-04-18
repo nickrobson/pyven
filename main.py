070f02b0809ea51a7f3dacf0b9a004fd6d82a43c
Nick Robson <robson.nicholas1@gmail.com>
2016-04-18 16:12:36 +1000

    Authentication and multiple repository support, with separate authentication.

--- main.py
+++ main.py
@@ -9,6 +9,25 @@ from flask import Flask, request, session, redirect, url_for, abort, \
                     flash, render_template, Blueprint, g, send_file
 
 
+class Repo(object):
+
+    def __init__(self, data):
+        super(Repo, self).__init__()
+        self.data = data
+        self.browse = data.get('browse', True)
+        self.auth = data.get('auth', None)
+
+    def has_auth(self):
+        return self.auth is not None
+
+    def is_auth(self, username, password):
+        if not self.has_auth():
+            return True
+        if username is None or password is None:
+            return False
+        return self.auth.get(username) == password
+
+
 app = Flask('pyven')
 
 with open('config.json', 'r') as f:
@@ -21,6 +40,11 @@ valid_gid = re.compile(r'[a-zA-Z0-9.]+')
 valid_aid = re.compile(r'[a-zA-Z0-9]+')
 valid_vid = re.compile(r'[0-9]+(?:\.[0-9]+)*')
 
+cfg_repos = app.config['repositories']
+repos = dict()
+for repo in cfg_repos:
+    repos[repo] = Repo(cfg_repos[repo])
+
 
 @bp.route('/')
 def get_index():
@@ -28,49 +52,63 @@ def get_index():
 
 
 @bp.route('/content/')
-@bp.route('/content/<path:url>')
-def get_file(url=''):
+@bp.route('/upload/')
+@bp.route('/login/')
+def get_repos():
+    return render_template('repos.html', repos=repos)
+
+
+@bp.route('/content/<repo>/')
+@bp.route('/content/<repo>/<path:url>')
+def get_file(repo, url=''):
     if '..' in url or url.startswith('/'):
         abort(404)
-    fname = os.path.join('artifacts', url)
+    rep = repos[repo]
+    if not rep:
+        abort(404)
+    fname = os.path.join('artifacts', repo, url)
     if os.path.isfile(fname):
-        return send_file(fname)
+        ext = fname[fname.rfind('.')+1:]
+        mime = 'text/plain'
+        return send_file(fname, mime)
     elif os.path.isdir(fname):
-        ls = os.listdir(fname)
-        files = []
-        if not url.startswith('/'):
-            url = '/' + url
-        if not url.endswith('/'):
-            url += '/'
-        for file in ls:
-            fn = os.path.join(fname, file)
-            info = dict()
-            stat = os.stat(fn)
-            info['name'] = file
-            info['link'] = url_for('.get_file', url=url[1:] + file)
-            if os.path.isdir(fn):
-                info['type'] = 'directory'
-                info['name'] += '/'
-                info['link'] += '/'
-            elif os.path.isfile(fn):
-                info['type'] = 'file'
-                info['size'] = stat.st_size
-            files.append(info)
-        files = sorted(files, key=lambda f: f['type'])
-        if url != '/':
-            parent = url[1:-1]
-            if '/' in parent:
-                parent = parent[:parent.rfind('/')]
-            else:
-                parent = ''
-            finfo = {
-                        'name': '../',
-                        'link': url_for('.get_file', url=parent)
-                    }
-            if finfo['link'] != url_for('.get_file', url=''):
-                finfo['link'] += '/'
-            files.insert(0, finfo)
-        return render_template('dir.html', dir=url, files=files)
+        if rep.browse or (session.get('repo') == repo and session.get('username')):
+            ls = os.listdir(fname)
+            files = []
+            if not url.startswith('/'):
+                url = '/' + url
+            if not url.endswith('/'):
+                url += '/'
+            for file in ls:
+                fn = os.path.join(fname, file)
+                info = dict()
+                stat = os.stat(fn)
+                info['name'] = file
+                info['link'] = url_for('.get_file', repo=repo, url=url[1:] + file)
+                if os.path.isdir(fn):
+                    info['type'] = 'directory'
+                    info['name'] += '/'
+                    info['link'] += '/'
+                elif os.path.isfile(fn):
+                    info['type'] = 'file'
+                    info['size'] = stat.st_size
+                files.append(info)
+            files = sorted(files, key=lambda f: f['type'])
+            if url != '/':
+                parent = url[1:-1]
+                if '/' in parent:
+                    parent = parent[:parent.rfind('/')]
+                else:
+                    parent = ''
+                finfo = {
+                            'name': '../',
+                            'link': url_for('.get_file', repo=repo, url=parent)
+                        }
+                if finfo['link'] != url_for('.get_file', repo=repo, url=''):
+                    finfo['link'] += '/'
+                files.insert(0, finfo)
+            return render_template('dir.html', dir=url, files=files)
+        abort(403)
     abort(404)
 
 
@@ -83,6 +121,7 @@ def make_md5(fname):
     with open(fname + '.md5', "w+") as f:
         f.write(hash_md5)
 
+
 def make_sha1(fname):
     hash_sha1 = hashlib.sha1()
     with open(fname, "rb") as f:
@@ -93,8 +132,12 @@ def make_sha1(fname):
         f.write(hash_sha1)
 
 
-@bp.route('/upload/', methods=['GET', 'POST'])
-def upload():
+@bp.route('/upload/<repo>/', methods=['GET', 'POST'])
+def upload(repo):
+    if not repos[repo]:
+        abort(404)
+    if not session.get('username') or session.get('repo') != repo:
+        return redirect(url_for('.login', repo=repo))
     if request.method == 'GET':
         return render_template('upload.html')
     jar = request.files.get('jar')
@@ -107,8 +150,8 @@ def upload():
         va = valid_aid.match(aid)
         vv = valid_vid.match(vid)
         if vg and va and vv:
-            path = '%s/%s/%s' % (gid.replace('.', '/'), aid, vid)
-            path = os.path.join('artifacts', path)
+            p = os.path.join(*gid.split('.'))
+            path = os.path.join('artifacts', repo, p, aid, vid)
             jarpath = os.path.join(path, '%s-%s.jar' % (aid, vid))
             pompath = os.path.join(path, '%s-%s.pom' % (aid, vid))
             if not os.path.exists(os.path.dirname(jarpath)):
@@ -129,8 +172,45 @@ def upload():
             res.append('artifactId')
         if not vv:
             res.append('version')
-        return '{"error":%s}' % json.dumps(res)
-    return '{"error":"missing params"}'
+        return '{"invalid":%s}' % json.dumps(res)
+    return '{"invalid":"missing params"}'
+
+
+@bp.route('/login/<repo>/', methods=['GET', 'POST'])
+def login(repo):
+    rep = repos[repo]
+    if not rep:
+        abort(404)
+    if session.get(repo) == repo:
+        return redirect(url_for('.get_file', repo=repo))
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        form = request.form
+        user = form.get('username')
+        pword = form.get('password')
+        if user and pword:
+            if rep.is_auth(user, pword):
+                session['repo'] = repo
+                session['username'] = user
+                return redirect(url_for('.get_file', repo=repo))
+            else:
+                flash('Invalid username or password.')
+                return redirect(url_for('.login', repo=repo))
+        abort(400)
+
+
+@bp.route('/logout/')
+def logout():
+    session.pop('repo', None)
+    if session.pop('username', None):
+        flash('You have been logged out.')
+    return redirect(url_for('.show_all'))
+
+
+@app.errorhandler(403)
+def err_forbidden(e):
+    return render_template('error403.html'), 403
 
 
 @app.errorhandler(404)
